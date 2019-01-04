5f1c617377f1afc836c56c5b4aead64ca8b0ea0d
JionLin <250912986@qq.com>
2019-01-04 15:46:18 +0800

    1.

--- src/array/CustomerArray.java
+++ src/array/CustomerArray.java
@@ -1,7 +1,5 @@
 package array;
 
-import javax.sound.midi.Soundbank;
-
 /**
  * @program: dataStructure
  * @description: 自定义数组
@@ -10,268 +8,245 @@ import javax.sound.midi.Soundbank;
  **/
 public class CustomerArray {
 
-    // TODO: 2018/12/15  带参构造 无参构造 数组的容量 是否为空  元素个数
-    // TODO: 2018/12/15  向所有元素后添加一个新元素 向所有元素前添加一个新元素 在index索引的位置插入一个新元素e
-    // TODO: 2018/12/15  获取index索引位置的元素  修改index索引位置的元素为e tostring方法
-    // TODO: 2018/12/15 查找数组中元素e所在的索引，如果不存在元素e，则返回-1  从数组中删除index位置的元素, 返回删除的元素
-    // TODO: 2018/12/15 从数组中删除第一个元素, 返回删除的元素  从数组中删除最后一个元素, 返回删除的元素 从数组中删除元素e
-    // TODO: 2018/12/15 泛型数据结构
-    // TODO: 2018/12/15  动态数组
-    // TODO: 2018/12/15  时间复杂度分析
+	private int[] data;
+	//	获取数组中的元素个数
+	private int size;
 
 
-    private int[] data;
-    //	获取数组中的元素个数
-    private int size;
+	/**
+	 * @Description: 带参构造
+	 * @Param: [capacity]
+	 * @return:
+	 * @Author: Join
+	 * @Date:
+	 */
+	public CustomerArray(int capacity) {
+		data = new int[capacity];
+		size = 0;
+	}
 
 
-    /**
-     * @Description: 带参构造
-     * @Param: [capacity]
-     * @return:
-     * @Author: Join
-     * @Date:
-     */
-    public CustomerArray(int capacity) {
-        data = new int[capacity];
-        size = 0;
-    }
+	/**
+	 * @Description: 无参构造
+	 * @Param: []
+	 * @return:
+	 * @Author: Join
+	 * @Date:
+	 */
+	public CustomerArray() {
+		this(10);
+	}
+
+	/**
+	 * @Description: 获取数组的容量大小
+	 * @Param: []
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 17:12
+	 */
+	public int getCapacity() {
+		return data.length;
+	}
+
+	/**
+	 * @Description: 获取数组中的元素个数
+	 * @Param: []
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 2018/12/19
+	 */
+	public int getSize() {
+		return size;
+	}
+
+	/**
+	 * @Description: 判断是否为空
+	 * @Param: []
+	 * @return: boolean
+	 * @Author: Join
+	 * @Date: 17:13
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
 
-    /**
-     * @Description: 无参构造
-     * @Param: []
-     * @return:
-     * @Author: Join
-     * @Date:
-     */
-    public CustomerArray() {
-        this(10);
-    }
-
-    /**
-     * @Description: 获取容量
-     * @Param: []
-     * @return: int
-     * @Author: Join
-     * @Date: 17:12
-     */
-    public int getCapacity() {
-        return data.length;
-    }
-
-    /** 
-    * @Description: 获取数组中的元素个数
-    * @Param: [] 
-    * @return: int 
-    * @Author: Join
-    * @Date: 2018/12/19 
-    */
-    public int getSize(){
-        return size;
-    }
-
-    /**
-     * @Description: 判断是否为空
-     * @Param: []
-     * @return: boolean
-     * @Author: Join
-     * @Date: 17:13
-     */
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    /**
-     * @Description: 获取数组的长度
-     * @Param: []
-     * @return: int
-     * @Author: Join
-     * @Date: 17:15
-     */
-    public int getLength() {
-        return data.length;
-    }
+	/**
+	 * @Description: 往所有元素后添加一个元素
+	 * @Param: [e]
+	 * @return: void
+	 * @Author: Join
+	 * @Date: 2018/12/15 17:58
+	 */
+	public void addLast(int e) {
+		//size 从0开始计算,带进来的,e为添加的哪个元素
+		add(size, e);
+	}
 
 
-    /**
-     * @Description: 往所有元素后添加一个元素
-     * @Param: [e]
-     * @return: void
-     * @Author: Join
-     * @Date: 2018/12/15 17:58
-     */
-    public void addLast(int e) {
-        //size 从0开始计算,带进来的,e为添加的哪个元素
-        add(size, e);
-    }
+	/**
+	 * @Description: 在index索引的位置插入一个新元素e
+	 * @Param: [index, e]
+	 * @return: void
+	 * @Author: Join
+	 * @Date: 2018/12/15 17:28
+	 */
+	public void add(int index, int e) {
+		// e  0 1  2 3 4
+		//判断size是否等于数组的长度
+		if (size == data.length) {
+			throw new IllegalArgumentException("add File size exception");
+		}
+		// 判断索引是否越界
+		if (index < 0 || index > size) {
+			throw new IllegalArgumentException("add File index exception");
+		}
+		for (int i = size - 1; i >= index; i--) {
+			data[i + 1] = data[i];
+		}
+		data[index] = e;
+		size++;
+	}
 
 
-    /**
-     * @Description: 在index索引的位置插入一个新元素e
-     * @Param: [index, e]
-     * @return: void
-     * @Author: Join
-     * @Date: 2018/12/15 17:28
-     */
-    public void add(int index, int e) {
-        // e  0 1  2 3 4
-        //判断size是否等于数组的长度
-        if (size == data.length) {
-            throw new IllegalArgumentException("add File size exception");
-        }
-        // 判断索引是否越界
-        if (index < 0 || index > size) {
-            throw new IllegalArgumentException("add File index exception");
-        }
-        for (int i = size - 1; i >= index; i--) {
-            data[i + 1] = data[i];
-        }
-        data[index] = e;
-        size++;
-    }
+	/**
+	 * @Description: 往第一个添加元素
+	 * @Param: [e]
+	 * @return: void
+	 * @Author: Join
+	 * @Date: 2018/12/15 18:00
+	 */
+	public void addFirst(int e) {
+		//0 1 2 3 4
+		add(0, e);
+	}
+
+	/**
+	 * @Description: 获取index索引位置的元素
+	 * @Param: [index]
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 2018/12/16 8:30
+	 */
+	public int get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("get index exception");
+		}
+		return data[index];
+	}
+
+	/**
+	 * @Description: 修改index索引位置的元素为e
+	 * @Param: []
+	 * @return: void
+	 * @Author: Join
+	 * @Date: 2018/12/16 8:34
+	 */
+	public void set(int index, int e) {
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("get index exception");
+		}
+		data[index] = e;
+	}
+
+	/**
+	 * @Description: 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+	 * @Param: [e]
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 2018/12/16 9:02
+	 */
+	public int getIndex(int e) {
+		for (int i = 0; i < size; i++) {
+			if (data[i] == e) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 
-    /**
-     * @Description: 往第一个添加元素
-     * @Param: [e]
-     * @return: void
-     * @Author: Join
-     * @Date: 2018/12/15 18:00
-     */
-    public void addFirst(int e) {
-        //0 1 2 3 4
-        add(0, e);
-    }
+	/**
+	 * @Description: 从数组中删除index位置的元素, 返回删除的元素
+	 * @Param: [index, e]
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 2018/12/16 9:14
+	 */
+	public int remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("get index exception");
+		}
+		int result = data[index];
+		for (int i = index + 1; i < size; i++) {
+			data[i - 1] = data[i];
+		}
+		size--;
+		return result;
+	}
 
-    /**
-     * @Description: 获取index索引位置的元素
-     * @Param: [index]
-     * @return: int
-     * @Author: Join
-     * @Date: 2018/12/16 8:30
-     */
-    public int get(int index) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("get index exception");
-        }
-        return data[index];
-    }
+	/**
+	 * @Description: 从数组中删除第一个元素, 返回删除的元素
+	 * @Param: [index]
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 2018/12/16 13:50
+	 */
+	public int removeFirst() {
+		return remove(0);
+	}
 
-    /**
-     * @Description: 修改index索引位置的元素为e
-     * @Param: []
-     * @return: void
-     * @Author: Join
-     * @Date: 2018/12/16 8:34
-     */
-    public void set(int index, int e) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("get index exception");
-        }
-        data[index] = e;
-    }
+	/**
+	 * @Description: 从数组中删除最后一个元素, 返回删除的元素
+	 * @Param: [index]
+	 * @return: int
+	 * @Author: Join
+	 * @Date: 2018/12/16 13:51
+	 */
+	public int removeLast() {
+		return remove(size - 1);
+	}
 
-    /**
-     * @Description: 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
-     * @Param: [e]
-     * @return: int
-     * @Author: Join
-     * @Date: 2018/12/16 9:02
-     */
-    public int getIndex(int e) {
-        for (int i = 0; i < size; i++) {
-            if (data[i] == e) {
-                return i;
-            }
-        }
-        return -1;
-    }
+	/**
+	 * @Description: 重写object的toString方法
+	 * @Param: []
+	 * @return: java.lang.String
+	 * @Author: Join
+	 * @Date: 2018/12/16 8:29
+	 */
+	@Override
+	public String toString() {
+//		%d和%f分别用来表示输出时，替换整型输出和浮点型输出的占位符
 
+		StringBuilder res = new StringBuilder();
+		res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+		res.append('[');
+		for (int i = 0; i < size; i++) {
+			res.append(data[i]);
+			if (i != size - 1) {
+				res.append(", ");
+			}
+		}
+		res.append(']');
+		return res.toString();
+	}
 
-    /**
-     * @Description: 从数组中删除index位置的元素, 返回删除的元素
-     * @Param: [index, e]
-     * @return: int
-     * @Author: Join
-     * @Date: 2018/12/16 9:14
-     */
-    public int remove(int index) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("get index exception");
-        }
-        int result = data[index];
-        for (int i = index + 1; i < size; i++) {
-            data[i - 1] = data[i];
-        }
-        size--;
-        return result;
-    }
+	public static void main(String[] args) {
+		CustomerArray array = new CustomerArray();
+		for (int i = 0; i < 5; i++) {
+			array.addLast(i);
+		}
+		System.out.println(array);
 
-    /**
-     * @Description: 从数组中删除第一个元素, 返回删除的元素
-     * @Param: [index]
-     * @return: int
-     * @Author: Join
-     * @Date: 2018/12/16 13:50
-     */
-    public int removeFirst(int index) {
-        return remove(0);
-    }
-
-    /**
-     * @Description: 从数组中删除最后一个元素, 返回删除的元素
-     * @Param: [index]
-     * @return: int
-     * @Author: Join
-     * @Date: 2018/12/16 13:51
-     */
-    public int removeLast(int index) {
-        return remove(size - 1);
-    }
-
-    /**
-     * @Description: 重写object的toString方法
-     * @Param: []
-     * @return: java.lang.String
-     * @Author: Join
-     * @Date: 2018/12/16 8:29
-     */
-    @Override
-    public String toString() {
-
-        StringBuilder res = new StringBuilder();
-        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
-        res.append('[');
-        for (int i = 0; i < size; i++) {
-            res.append(data[i]);
-            if (i != size - 1) {
-                res.append(", ");
-            }
-        }
-        res.append(']');
-        return res.toString();
-    }
-
-    public static void main(String[] args) {
-        CustomerArray array = new CustomerArray();
-        for (int i = 0; i < 5; i++) {
-            array.addLast(i);
-        }
-        System.out.println(array);
-        System.out.println(array.get(3));
-        array.set(2, 100);
-        System.out.println(array);
-        System.out.println("getIndex= " + array.getIndex(4));
-        System.out.println("remove= " + array.remove(2));
-        System.out.println(array);
-        System.out.println("============");
-        System.out.println(array.removeFirst(0));
-        System.out.println(array);
-        System.out.println("============");
-        System.out.println(array.removeLast(array.size - 1));
-        System.out.println(array);
-    }
+		array.set(2, 100);
+		System.out.println(array);
 
 
+		array.removeFirst();
+		System.out.println(array);
+
+		array.removeLast();
+		System.out.println(array);
+
+	}
 }
--- src/quene/CustomerGenericArray.java
+++ src/quene/CustomerGenericArray.java
@@ -8,158 +8,158 @@ package quene;
  **/
 public class CustomerGenericArray<E> {
 
-    //长度
-    public int size;
-    //数组
-    public E[] data;
+	//长度
+	public int size;
+	//数组
+	public E[] data;
 
-    //自定义构造方法
-    public CustomerGenericArray(int capacity) {
-        data = (E[]) new Object[capacity];
-        size = 0;
-    }
+	//自定义构造方法
+	public CustomerGenericArray(int capacity) {
+		data = (E[]) new Object[capacity];
+		size = 0;
+	}
 
-    //无参构造
-    public CustomerGenericArray() {
-        this(10);
-    }
+	//无参构造
+	public CustomerGenericArray() {
+		this(10);
+	}
 
-    //获取数组的容量 1
-    public int getCapacity() {
-        return data.length;
-    }
+	//获取数组的容量 1
+	public int getCapacity() {
+		return data.length;
+	}
 
-    //获取数组中的元素个数 2
-    public int getSize() {
-        return size;
-    }
+	//获取数组中的元素个数 2
+	public int getSize() {
+		return size;
+	}
 
-    //返回数组是否为空 3
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	//返回数组是否为空 3
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    //在index索引的位置插入一个新元素e 4
-    public void addElement(int index, E e) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("索引异常");
-        }
-        if (size == data.length) {
-            resize(data.length * 2);
-        }
-        for (int i = size - 1; i >= index; i--) {
-            data[i + 1] = data[i];
-        }
-        data[index] = e;
-        size++;
+	//在index索引的位置插入一个新元素e 4
+	public void addElement(int index, E e) {
+		if (index < 0 || index > size) {
+			throw new IllegalArgumentException("索引异常");
+		}
+		if (size == data.length) {
+			resize(data.length * 2);
+		}
+		for (int i = size - 1; i >= index; i--) {
+			data[i + 1] = data[i];
+		}
+		data[index] = e;
+		size++;
 
-    }
+	}
 
-    //向所有元素后添加一个新元素 5
-    public void addLast(E e) {
-        addElement(size - 1, e);
-    }
+	//向所有元素后添加一个新元素 5
+	public void addLast(E e) {
+		addElement(size, e);
+	}
 
-    //在所有元素前添加一个新元素 6
-    public void addFirst(E e) {
-        addElement(0, e);
-    }
+	//在所有元素前添加一个新元素 6
+	public void addFirst(E e) {
+		addElement(0, e);
+	}
 
-    //获取index索引位置的元素 7
-    public E getElement(int index) {
-        if (index < 0 || index > size - 1) {
-            throw new IllegalArgumentException("索引异常");
-        }
-        return data[index];
-    }
+	//获取index索引位置的元素 7
+	public E getElement(int index) {
+		if (index < 0 || index > size - 1) {
+			throw new IllegalArgumentException("索引异常");
+		}
+		return data[index];
+	}
 
-    //修改index索引位置的元素e 8
-    public void set(int index, E e) {
-        if (index < 0 || index > size - 1) {
-            throw new IllegalArgumentException("索引异常");
-        }
-        data[index] = e;
-    }
+	//修改index索引位置的元素e 8
+	public void set(int index, E e) {
+		if (index < 0 || index > size - 1) {
+			throw new IllegalArgumentException("索引异常");
+		}
+		data[index] = e;
+	}
 
-    //查找数组中是否有元素e 9
-    public boolean isContain(E e) {
-        for (int i = 0; i < data.length; i++) {
-            if (data[i].equals(e)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	//查找数组中是否有元素e 9
+	public boolean isContain(E e) {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i].equals(e)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    //查找数组中元素e所在的索引,如果没有,返回-1  10
-    public int find(E e) {
-        for (int i = 0; i < data.length; i++) {
-            if (data[i].equals(e)) {
-                return i;
-            }
-        }
+	//查找数组中元素e所在的索引,如果没有,返回-1  10
+	public int find(E e) {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i].equals(e)) {
+				return i;
+			}
+		}
 
-        return -1;
-    }
+		return -1;
+	}
 
-    //从数组中删除index位置的元素,返回删除的元素 11
-    public E removeElement(int index) {
-        if (index < 0 || index >= size) {
-            throw new IllegalArgumentException("索引异常");
-        }
-        if (size == data.length / 4 && size != data.length / 2) {
-            resize(data.length / 2);
-        }
-        for (int i = size - 1; i >= index; i--) {
-            data[i - 1] = data[i];
-        }
-        size--;
-        return data[index];
-    }
+	//从数组中删除index位置的元素,返回删除的元素 11
+	public E removeElement(int index) {
+		if (index < 0 || index >= size) {
+			throw new IllegalArgumentException("索引异常");
+		}
+		if (size == data.length / 4 && size != data.length / 2) {
+			resize(data.length / 2);
+		}
+		for (int i = size - 1; i >= index; i--) {
+			data[i - 1] = data[i];
+		}
+		size--;
+		return data[index];
+	}
 
-    //从数组中删除第一个元素,返回删除的元素 12
-    public void removeFirst() {
-        removeElement(0);
-    }
+	//从数组中删除第一个元素,返回删除的元素 12
+	public void removeFirst() {
+		removeElement(0);
+	}
 
-    //从数组中删除最后一个元素,返回删除的元素 13
-    public E removeLast() {
-        return removeElement(size - 1);
-    }
+	//从数组中删除最后一个元素,返回删除的元素 13
+	public E removeLast() {
+		return removeElement(size - 1);
+	}
 
-    //toString方法 14 Array :  size=  length=   [1,2,3]
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("CustomerGenericArray: size: %d,length: %d", size, data.length);
-        builder.append("[");
-        for (int i = 0; i < data.length; i++) {
-            builder.append(data[i] + " ,");
-            if (data.length == size - 1) {
-                builder.append(data[i]);
-            }
-        }
-        builder.append("]");
-        return builder.toString();
-    }
+	//toString方法 14 Array :  size=  length=   [1,2,3]
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CustomerGenericArray: size: %d,length: %d", size, data.length);
+		builder.append("[");
+		for (int i = 0; i < data.length; i++) {
+			builder.append(data[i] + " ,");
+			if (data.length == size - 1) {
+				builder.append(data[i]);
+			}
+		}
+		builder.append("]");
+		return builder.toString();
+	}
 
-    //将数组空间的容量变成newCapacity大小 15  todo
-    public void resize(int newCapacity) {
-        E[] newData = (E[]) new Object[newCapacity];
-        for (int i = 0; i < size; i++) {
-            newData[i] = data[i];
-        }
-        data = newData;
-    }
+	//将数组空间的容量变成newCapacity大小 15  todo
+	public void resize(int newCapacity) {
+		E[] newData = (E[]) new Object[newCapacity];
+		for (int i = 0; i < size; i++) {
+			newData[i] = data[i];
+		}
+		data = newData;
+	}
 
-    //获取最后一个
-    public E getLast() {
-        return getElement(size - 1);
-    }
+	//获取最后一个
+	public E getLast() {
+		return getElement(size - 1);
+	}
 
-    //获取第一个
-    public E getFirst() {
-        return getElement(0);
-    }
+	//获取第一个
+	public E getFirst() {
+		return getElement(0);
+	}
 
 }
